9aab98f6c157577cfbaaeac97e17b985404641b9
ashiver <anthony.lee.shiver@gmail.com>
2015-04-10 19:17:49 +0000

    Updated db address

--- blog/config.py
+++ blog/config.py
@@ -1,5 +1,5 @@
 import os
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgres://wogrykqpgkbtcg:tiSV9G5dPrd9op-OvgsCrp5IpO@ec2-107-20-159-103.compute-1.amazonaws.com:5432/d7b0dda2r1ck1f"
+    SQLALCHEMY_DATABASE_URI = "postgres://jakauqgzidtdtx:VcXGQjs7H7EnbqnnFrFQTN_cGj@ec2-54-163-226-9.compute-1.amazonaws.com:5432/d79fao3btq7b6k"
     DEBUG = True
     SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", "99problemsbutascootsaintone")
