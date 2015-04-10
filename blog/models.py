cd098f110012d76b4d1505e773a2737ba66c0bc6
ashiver <anthony.lee.shiver@gmail.com>
2015-04-10 16:31:48 +0000

    Added authentication and login features

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 import datetime
-
-from sqlalchemy import Column, Integer, String, Text, DateTime
-
+from flask.ext.login import UserMixin
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from .database import Base, engine
 
 class Post(Base):
@@ -11,5 +11,15 @@ class Post(Base):
     title = Column(String(1024))
     content = Column(Text)
     datetime = Column(DateTime, default=datetime.datetime.now)
+    author_id = Column(Integer, ForeignKey('users.id'))
+    
+class User(Base, UserMixin):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    email = Column(String(128), unique=True)
+    password = Column(String(128))
+    posts = relationship("Post", backref="author")
     
 Base.metadata.create_all(engine)
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 import mistune
+from flask.ext.login import login_user, login_required, current_user
+from werkzeug.security import check_password_hash
 
 from blog import app
 from .database import session
-from .models import Post
+from .models import Post, User
 
 
 @app.route("/")
@@ -34,20 +36,29 @@ def posts(page=1, paginate_by=10):
                           )
 
 
+
+
 @app.route("/post/add", methods=["GET"])
+@login_required
 def add_post_get():
     return render_template("add_post.html")
 
 
+
+
 @app.route("/post/add", methods=["POST"])
+@login_required
 def add_post_post():
     post = Post(
         title=request.form["title"],
         content=mistune.markdown(request.form["content"]),
+        author=current_user
     )
     session.add(post)
     session.commit()
     return redirect(url_for("posts"))
+
+
 
 
 # My Routes
@@ -60,6 +71,7 @@ def post_id_get(id):
                           )
 
 @app.route("/post/<id>/edit", methods=["GET"])
+@login_required
 def edit_post_get(id):
     post = session.query(Post).get(id)
     return render_template("edit_post.html",
@@ -68,6 +80,7 @@ def edit_post_get(id):
 
 
 @app.route("/post/<id>/edit", methods=["POST"])
+@login_required
 def edit_post_post(id):
     post = session.query(Post).get(id)
     post.title = request.form["title"],
@@ -79,16 +92,42 @@ def edit_post_post(id):
 
 
 @app.route("/post/<id>/delete", methods=["GET"])
+@login_required
 def delete_post_get(id):
     post = session.query(Post).get(id)
     return render_template("delete_post.html",
                           post=post
                           )
 
+
+
+
 @app.route("/post/<id>/delete", methods=["POST"])
+@login_required
 def delete_post_delete(id):
     post = session.query(Post).get(id)
     session.delete(post)
     session.commit()
     return redirect(url_for("posts"))
 
+
+
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+
+
+
+
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("posts"))
