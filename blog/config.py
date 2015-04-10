7b216de653dd70e90166527cf2b0435a85143a2d
ashiver <anthony.lee.shiver@gmail.com>
2015-04-10 18:40:55 +0000

    Updated to use Heroku db

--- blog/config.py
+++ blog/config.py
@@ -1,5 +1,5 @@
 import os
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://action:action@localhost:5432/blogful"
+    SQLALCHEMY_DATABASE_URI = "postgres://wogrykqpgkbtcg:tiSV9G5dPrd9op-OvgsCrp5IpO@ec2-107-20-159-103.compute-1.amazonaws.com:5432/d7b0dda2r1ck1f"
     DEBUG = True
     SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", "99problemsbutascootsaintone")
