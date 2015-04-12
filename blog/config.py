99c70587f1e6d19f1e8d8e57fe21934bf0377d9c
ashiver <anthony.lee.shiver@gmail.com>
2015-04-12 16:04:26 +0000

    Secured db

--- blog/config.py
+++ blog/config.py
@@ -1,5 +1,5 @@
 import os
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgres://hpulpkzdcudodr:c-hkq21sRJJ2fmujfAP7w5oT5I@ec2-54-197-238-8.compute-1.amazonaws.com:5432/dc1p7pp3qag615"
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     DEBUG = True
     SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", "99problemsbutascootsaintone")
